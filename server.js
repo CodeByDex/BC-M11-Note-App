7eaeb6f127bf3ef3174943dc7d0f84a955f2606a
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-10 18:14:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 const api = require("./lib/API");
 
 const app = express();
