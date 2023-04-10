990cbde35f3cba36e06e333228ccfb99d4400658
Ryan <119005046+CodeByDex@users.noreply.github.com>
2023-04-10 15:07:47 -0400

    reordered routing for wild card usage for index.html

--- Develop/server.js
+++ Develop/server.js
@@ -12,12 +12,13 @@ app.use(express.static("public"));
 
 app.use("/api", api);
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-})
-
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+
+//Need to register the wild card last so that more specific routes are used first.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
 app.listen(PORT, () => {
